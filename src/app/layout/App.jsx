4135246319e225b48ef582d8e8e0187f450a264f
wolfwar <wolfwar@outlook.com>
2019-10-23 12:54:31 +0200

    napravio route

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,16 +2,44 @@ import React, { Component, Fragment } from "react";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import EventForm from "../../features/event/EventForm/EventForm";
 
+/*
+razbio sam rute na dva dijela
+prvi je samo sa '/' - home page
+a ako nije, podrazumjeva se da je bilo koja od ostalih
+render metoda (umjesto component) ne prenosi automatski propse... 
+tako da ih moram ručno kao argumente od arrow funk. prenjeti
+sintaksa za bilo koji PATH je path="/(.+)"
+*/
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
-      </Fragment>
+      <>
+        <Route exact path="/" component={HomePage} />
+        <Route
+          path="/(.+)"
+          render={() => (
+            <Fragment>
+              <NavBar />
+              <Container className="main">
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/events/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingsDashboard} />
+                <Route path="/createEvent" component={EventForm} />
+              </Container>
+            </Fragment>
+          )}
+        />
+      </>
     );
   }
 }
---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PeopleDashboard = () => {
+  return (
+    <div>
+      <h1>people dashboard page</h1>
+    </div>
+  );
+};
+
+export default PeopleDashboard;
